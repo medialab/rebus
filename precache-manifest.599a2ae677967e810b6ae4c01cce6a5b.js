1470344fcec3b130fda84d6f8343e566fb34be6d
Robin de Mourat <robin.demourat@gmail.com>
2019-02-20 16:50:17 +0100

    Updates

--- precache-manifest.599a2ae677967e810b6ae4c01cce6a5b.js
+++ precache-manifest.599a2ae677967e810b6ae4c01cce6a5b.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/rebus/static/js/runtime~main.7b26460a.js"
   },
   {
-    "revision": "138cca4a5ff372b8b4d3",
-    "url": "/rebus/static/js/main.138cca4a.chunk.js"
+    "revision": "f42e49fbe29087ad0715",
+    "url": "/rebus/static/js/main.f42e49fb.chunk.js"
   },
   {
     "revision": "237faa555ba7498389ad",
     "url": "/rebus/static/js/1.237faa55.chunk.js"
   },
   {
-    "revision": "138cca4a5ff372b8b4d3",
+    "revision": "f42e49fbe29087ad0715",
     "url": "/rebus/static/css/main.4c37746c.chunk.css"
   },
   {
-    "revision": "cd7b54a03fd7b1cc260a77713cd2c633",
+    "revision": "46be3f78eacde4b1caae856a7aee0ae3",
     "url": "/rebus/index.html"
   }
 ];
