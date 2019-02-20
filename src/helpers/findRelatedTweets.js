b46490721cb41def71f856898442a3c2bc3ff119
Robin de Mourat <robin.demourat@gmail.com>
2019-02-20 16:06:54 +0100

    improve tweet retrieval mode

--- src/helpers/findRelatedTweets.js
+++ src/helpers/findRelatedTweets.js
@@ -3,8 +3,8 @@
 const findRelatedTweets = (tweets, targetTerm) => {
   const term = targetTerm.toLowerCase();
   return tweets.filter(tweet => {
-    const text = tweet.text.toLowerCase();
-    return text.includes(' ' + term);
+    const text = tweet.clean.toLowerCase();
+    return text.includes(term);
   })
 }
 
